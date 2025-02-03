fcaa5e6fd97177f5dd262d632db6d39f7b8c6f55
Antony B <antony.baasan@gmail.com>
2025-02-03 10:35:30 -0500

    debug

--- src/app/actions/addToWaitingList.ts
+++ src/app/actions/addToWaitingList.ts
@@ -2,12 +2,12 @@
 
 import mailchimp from "@mailchimp/mailchimp_marketing";
 
-mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API,
-  server: process.env.MAILCHIMP_SERVER,
-});
 
 export async function addToWaitingList(formData: FormData) {
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API,
+    server: process.env.MAILCHIMP_SERVER,
+  });
 
   console.log('Added to waiting list:', formData);
   const email = formData.get('email')?.toString();
@@ -16,6 +16,8 @@ export async function addToWaitingList(formData: FormData) {
     throw new Error('Email is required');
   }
   const listId = process.env.MAILCHIMP_LIST_ID;
+  console.log('listId', listId);
+  console.log('server', process.env.MAILCHIMP_SERVER);
 
   if (!listId) {
     throw new Error('Mailchimp List ID is not configured');
@@ -30,8 +32,7 @@ export async function addToWaitingList(formData: FormData) {
     });
     console.log('mailchimp response: ', response);
   } catch (error) {
-    console.error('Error adding to waiting list:', error);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    console.error('Error adding to waiting list:', (error as any).response);
   }
-
-
 }
